223b0902dc39379643638e30d0d069c2cbdabcb9
Ruslan Sabirov <rsabirov22@yandex.com>
2022-10-08 19:50:54 +0600

    feature: header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import logo from '../../images/header-logo.svg';
 import './Header.css';
 
 function Header (props) {
 
     return (
-        <header className={props.className}>
+        <header className="header">
           <div className="page__container">
-            Header
+            <div className="header__container">
+              <a className="header__logo-container" href="/">
+                <img className="header__logo" src={logo} alt="лого"/> 
+              </a>
+              <div className="header__authorize">
+                <a className="header__signup" href="/signup">Регистрация</a>
+                <button className="header__signin" type='button'>Войти</button>
+              </div>
+            </div>
           </div>
         </header>             
     );
